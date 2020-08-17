68f28a62e06ed201aca6fb9eee09f26f492359ee
Duyệt Khắc <you@example.com>
2020-08-17 09:33:55 +0700

    hnew 9h30 17/8/2020

--- resources/views/backEnd/index.blade.php
+++ resources/views/backEnd/index.blade.php
@@ -6,7 +6,7 @@
 			<div class="col-lg-12">
 				<h1 class="page-header">Trang chủ</h1>
 			</div>
-		</div><!--/.row-->
+		</div>
 									
 		<div class="row">
 			<div class="col-xs-12 col-md-6 col-lg-3">
@@ -16,8 +16,8 @@
 							<svg class="glyph stroked bag"><use xlink:href="#stroked-bag"></use></svg>
 						</div>
 						<div class="col-sm-9 col-lg-7 widget-right">
-							<div class="large">120</div>
-							<div class="text-muted">Sản phẩm</div>
+							<div class="large">{{$tour}}</div>
+							<div class="text-muted">Tour</div>
 						</div>
 					</div>
 				</div>
@@ -30,8 +30,8 @@
 							<svg class="glyph stroked empty-message"><use xlink:href="#stroked-empty-message"></use></svg>
 						</div>
 						<div class="col-sm-9 col-lg-7 widget-right">
-							<div class="large">52</div>
-							<div class="text-muted">Bình luận</div>
+							<div class="large">{{$review}}</div>
+							<div class="text-muted">Review</div>
 						</div>
 					</div>
 				</div>
@@ -43,8 +43,8 @@
 							<svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg>
 						</div>
 						<div class="col-sm-9 col-lg-7 widget-right">
-							<div class="large">24</div>
-							<div class="text-muted">Người dùng</div>
+							<div class="large">{{$user}}</div>
+							<div class="text-muted">User</div>
 						</div>
 					</div>
 				</div>
@@ -56,23 +56,23 @@
 							<svg class="glyph stroked app-window-with-content"><use xlink:href="#stroked-app-window-with-content"></use></svg>
 						</div>
 						<div class="col-sm-9 col-lg-7 widget-right">
-							<div class="large">25.2k</div>
-							<div class="text-muted">Danh mục</div>
+							<div class="large">{{$dest}}</div>
+							<div class="text-muted">Destination</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div><!--/.row-->
+		</div>
 		
 		<div class="row">
 			<div class="col-md-12">
 				<div class="panel panel-red">
-					<div class="panel-heading dark-overlay"><svg class="glyph stroked calendar"><use xlink:href="#stroked-calendar"></use></svg>Lịch</div>
+					<div class="panel-heading dark-overlay"><svg class="glyph stroked calendar"><use xlink:href="#stroked-calendar"></use></svg>Calendar</div>
 					<div class="panel-body">
 						<div id="calendar"></div>
 					</div>
 				</div>
-			</div><!--/.col-->
-		</div><!--/.row-->
-	</div>	<!--/.main-->
+			</div>
+		</div>
+	</div>	
 @stop
